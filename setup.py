76110c49c489706ca8da6c6025408ee11207f04b
happyday <bitknitting@gmail.com>
2020-03-28 08:21:20 -0700

    Bump version: 0.0.8 → 0.0.9

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/bitknitting/FHmonitor',
-    version='0.0.8',
+    version='0.0.9',
     zip_safe=False,
     entry_points={
         'console_scripts': [
